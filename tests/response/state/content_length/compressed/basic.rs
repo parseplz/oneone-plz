46728583e08164113706f4748bbdeaef24a22f3e
darkseid <gh0stv3g3ta@gmail.com>
2025-07-10 11:01:27 +0530

    refactor: organized test

--- tests/response/state/content_length/compressed/basic.rs
+++ tests/response/state/content_length/compressed/basic.rs
@@ -39,3 +39,52 @@ fn test_response_state_content_length_zstd() {
                   hello world";
     assert_eq!(response.into_bytes(), verify);
 }
+
+#[test]
+fn test_response_decompress_all_single() {
+    let compressed = compressed_data();
+    let mut input: Vec<u8> = format!(
+        "HTTP/1.1 200 OK\r\n\
+        Host: reqbin.com\r\n\
+        Content-Type: text/plain\r\n\
+        Content-Length: {}\r\n\
+        Content-Encoding: br, deflate, gzip, zstd\r\n\r\n",
+        compressed.len()
+    )
+    .into();
+    input.extend_from_slice(&compressed[..]);
+    let result = poll_oneone_only_read::<Response>(&input);
+    let verify = "HTTP/1.1 200 OK\r\n\
+                  Host: reqbin.com\r\n\
+                  Content-Type: text/plain\r\n\
+                  Content-Length: 11\r\n\r\n\
+                  hello world";
+
+    assert_eq!(result.into_bytes(), verify);
+}
+
+#[test]
+fn test_response_decompress_all_multiple() {
+    let compressed = compressed_data();
+    let mut input: Vec<u8> = format!(
+        "HTTP/1.1 200 OK\r\n\
+        Content-Encoding: br\r\n\
+        Host: reqbin.com\r\n\
+        Content-Encoding: deflate\r\n\
+        Content-Type: text/plain\r\n\
+        Content-Encoding: gzip \r\n\
+        Content-Length: {}\r\n\
+        Content-Encoding: zstd\r\n\r\n",
+        compressed.len()
+    )
+    .into();
+    input.extend_from_slice(&compressed[..]);
+    let result = poll_oneone_only_read::<Response>(&input);
+    let verify = "HTTP/1.1 200 OK\r\n\
+                  Host: reqbin.com\r\n\
+                  Content-Type: text/plain\r\n\
+                  Content-Length: 11\r\n\r\n\
+                  hello world";
+
+    assert_eq!(result.into_bytes(), verify);
+}
