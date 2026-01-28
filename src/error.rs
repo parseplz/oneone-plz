18be521ed7d385df99a9a2a5d33ac7bd9dd67c2b
darkseid <gh0stv3g3ta@gmail.com>
2026-01-28 16:30:19 +0530

    feat: clippy - use Box in HttpStateError

--- src/error.rs
+++ src/error.rs
@@ -4,18 +4,16 @@ use decompression_plz::{
     DecompressTrait, content_length::update_content_length,
 };
 use header_plz::{
-    InfoLine, body_headers::parse::ParseBodyHeaders, error::HeaderReadError,
-    message_head::MessageHead,
+    OneHeader, OneInfoLine, body_headers::parse::ParseBodyHeaders,
+    error::HeaderReadError, message_head::MessageHead,
 };
-use protocol_traits_plz::Frame;
+use http_plz::OneOne;
 use thiserror::Error;
-
-use crate::oneone::OneOne;
 
 #[derive(Debug, Error)]
 pub enum HttpStateError<T>
 where
-    T: InfoLine,
+    T: OneInfoLine,
 {
     #[error("header read| {0}")]
     InfoLine(#[from] HeaderReadError),
@@ -25,21 +23,22 @@ where
     #[error("partial| header")]
     Unparsed(BytesMut),
     #[error("partial| content length")]
-    ContentLengthPartial(OneOne<T>, BytesMut),
+    ContentLengthPartial(Box<(OneOne<T>, BytesMut)>),
     #[error("header not enough data")]
-    ChunkReaderPartial(OneOne<T>, BytesMut),
+    ChunkReaderPartial(Box<(OneOne<T>, BytesMut)>),
 }
 
 impl<T> From<HttpStateError<T>> for BytesMut
 where
-    T: InfoLine,
-    MessageHead<T>: ParseBodyHeaders,
+    T: OneInfoLine + std::fmt::Debug,
+    MessageHead<T, OneHeader>: ParseBodyHeaders,
 {
     fn from(value: HttpStateError<T>) -> Self {
         match value {
             HttpStateError::Unparsed(buf) => buf,
-            HttpStateError::ContentLengthPartial(oneone, buf)
-            | HttpStateError::ChunkReaderPartial(oneone, buf) => {
+            HttpStateError::ContentLengthPartial(boxed)
+            | HttpStateError::ChunkReaderPartial(boxed) => {
+                let (oneone, buf) = *boxed;
                 let mut data = oneone.into_bytes();
                 data.unsplit(buf);
                 data
@@ -51,14 +50,15 @@ where
 
 impl<T> TryFrom<HttpStateError<T>> for OneOne<T>
 where
-    T: InfoLine,
-    MessageHead<T>: ParseBodyHeaders,
+    T: OneInfoLine,
+    MessageHead<T, OneHeader>: ParseBodyHeaders,
 {
     type Error = HttpStateError<T>;
 
     fn try_from(value: HttpStateError<T>) -> Result<Self, Self::Error> {
         match value {
-            HttpStateError::ContentLengthPartial(mut oneone, buf) => {
+            HttpStateError::ContentLengthPartial(boxed) => {
+                let (mut oneone, buf) = *boxed;
                 let len = buf.len();
                 oneone.set_body(Body::Raw(buf));
                 update_content_length(&mut oneone, len);
